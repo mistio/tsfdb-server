60371ac7741a82504e707ede0ec5c177988e3b39
Vasileios Papavasileiou <vpapav96@gmail.com>
2020-05-05 16:57:22 +0300

    Simplify queue

--- consumer.py
+++ consumer.py
@@ -1,18 +1,24 @@
+import fdb
+import os
 from time import sleep
 
 from tsfdb_server_v1.controllers.db import open_db, write_in_kv
-from tsfdb_server_v1.controllers.queue import Queue, Subspace
+from tsfdb_server_v1.controllers.queue import Queue
 
 
 def main():
     db = open_db()
-    queue = Queue(Subspace(('queue',)))
+    num = os.urandom(5)
+    queue = Queue(fdb.Subspace(('queue',)))
     while True:
-        data = queue.pop(db)
-        if data:
-            write_in_kv(data)
-        else:
-            sleep(1)
+        try:
+            data = queue.pop(db)
+            if data:
+                write_in_kv(data)
+            else:
+                sleep(1)
+        except fdb.FDBError as err:
+            print(err.description)
 
 
 if __name__ == "__main__":
