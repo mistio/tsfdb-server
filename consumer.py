6d58e86f20340fa95028b82296e9d6eca4d9b891
Vasileios Papavasileiou <vpapav96@gmail.com>
2020-05-14 19:20:26 +0300

    Handle edge cases

--- consumer.py
+++ consumer.py
@@ -11,24 +11,19 @@ RETRY_TIMEOUT = 5
 
 @fdb.transactional
 def acquire_queue(tr, available_queues):
-    try:
-        for k, v in tr[available_queues.range()]:
-            queue_name = fdb.tuple.unpack(k)[1]
-            consumer_lock = None
-            if tr[fdb.Subspace(('consumer_lock', queue_name))].present():
-                consumer_lock = fdb.tuple.unpack(
-                    tr[fdb.Subspace(('consumer_lock', queue_name))])[0]
-            if consumer_lock is None or \
-                    int(datetime.now().timestamp()) - \
-                    consumer_lock > ACQUIRE_TIMEOUT:
-                tr[fdb.Subspace(('consumer_lock', queue_name))] = \
-                    fdb.tuple.pack((int(datetime.now().timestamp()),))
-                return queue_name
-        return None
-    except fdb.FDBError as err:
-        if err.code != 1020:
-            tr.on_error(err.code).wait()
-        return None
+    for k, v in tr[available_queues.range()]:
+        queue_name = fdb.tuple.unpack(k)[1]
+        consumer_lock = None
+        if tr[fdb.Subspace(('consumer_lock', queue_name))].present():
+            consumer_lock = fdb.tuple.unpack(
+                tr[fdb.Subspace(('consumer_lock', queue_name))])[0]
+        if consumer_lock is None or \
+                int(datetime.now().timestamp()) - \
+                consumer_lock > ACQUIRE_TIMEOUT:
+            tr[fdb.Subspace(('consumer_lock', queue_name))] = \
+                fdb.tuple.pack((int(datetime.now().timestamp()),))
+            return queue_name
+    return None
 
 
 def consume_queue(db, acquired_queue):
@@ -45,13 +40,19 @@ def consume_queue(db, acquired_queue):
         except fdb.FDBError as err:
             if err.code != 1020:
                 db.on_error(err.code).wait()
+            return
 
 
 def main():
     db = open_db()
     available_queues = fdb.Subspace(('available_queues',))
     while True:
-        acquired_queue = acquire_queue(db, available_queues)
+        try:
+            acquired_queue = acquire_queue(db, available_queues)
+        except fdb.FDBError as err:
+            if err.code != 1020:
+                db.on_error(err.code).wait()
+            acquired_queue = None
         if acquired_queue:
             consume_queue(db, acquired_queue)
         else:
