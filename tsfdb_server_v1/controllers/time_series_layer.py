3d8ca2edde68a953b9019fdd067009159c96be7a
Vasileios Papavasileiou <vpapav96@gmail.com>
2020-07-01 14:14:59 +0300

    Don't cache dirs

--- tsfdb_server_v1/controllers/time_series_layer.py
+++ tsfdb_server_v1/controllers/time_series_layer.py
@@ -16,57 +16,15 @@ log = logging.getLogger(__name__)
 
 class TimeSeriesLayer():
     def __init__(self):
-        self.dir_monitoring = None
-        self.dir_orgs = {}
-        self.dir_resources = {}
-        self.dir_resources_resolutions = {}
-        self.dir_available_metrics = {}
         self.struct_types = (int, float)
-
-    def __set_monitoring_dir(self, db):
-        if not self.dir_monitoring:
-            self.dir_monitoring = fdb.directory.create_or_open(
-                db, ('monitoring',))
-
-    def __get_org_dir(self, db, org):
-        self.__set_monitoring_dir(db)
-        if not self.dir_orgs.get(org):
-            self.dir_orgs[org] = self.dir_monitoring.create_or_open(
-                db, (org,))
-        return self.dir_orgs[org]
-
-    def __get_resource_dir(self, db, org, resource):
-        if not self.dir_resources.get(org, {}).get(resource):
-            if not self.dir_resources.get(org):
-                self.dir_resources[org] = {}
-            self.dir_resources[org][resource] = self.__get_org_dir(
-                db, org).create_or_open(db, (resource,))
-        return self.dir_resources[org][resource]
-
-    def __get_available_metrics_dir(self, db, org):
-        if not self.dir_available_metrics.get(org):
-            self.dir_available_metrics[org] = self.__get_org_dir(
-                db, org).create_or_open(db, ('available_metrics',))
-        return self.dir_available_metrics[org]
-
-    def __get_resource_resolution_dir(self, db, org, resource, resolution):
-        if not self.dir_resources_resolutions.get(org, {}).get(
-                resource, {}).get(resolution):
-            if not self.dir_resources_resolutions.get(org):
-                self.dir_resources_resolutions[org] = {}
-            if not self.dir_resources_resolutions[org].get(resource):
-                self.dir_resources_resolutions[org][resource] = {}
-            self.dir_resources_resolutions[org][resource][resolution] = \
-                self.__get_resource_dir(db, org, resource).create_or_open(
-                    db, (resolution,))
-        return self.dir_resources_resolutions[org][resource][resolution]
 
     @fdb.transactional
     def find_metrics(self, tr, org, resource):
         metrics = {}
-        for k, v in tr.get_range_startswith(self.__get_available_metrics_dir(
-                tr, org).pack((resource,))):
-            metric = self.__get_available_metrics_dir(tr, org).unpack(k)[1]
+        available_metrics = fdb.directory.create_or_open(
+            tr, ('monitoring', org, 'available_metrics'))
+        for k, v in tr.get_range_startswith(available_metrics.pack((resource,))):
+            metric = available_metrics.unpack(k)[1]
             value = fdb.tuple.unpack(v)[0]
             metrics.update(metric_to_dict(metric, value))
         return metrics
@@ -75,7 +33,8 @@ class TimeSeriesLayer():
     def find_resources(self, tr, org, regex_resources,
                        authorized_resources=None):
         filtered_resources = []
-        resources = set(self.__get_org_dir(tr, org).list(tr))
+        resources = set(fdb.directory.create_or_open(
+            tr, ('monitoring', org)).list(tr))
         # Remove reserved directory for metrics
         resources.remove('available_metrics')
         # Use only authorized resources
@@ -131,22 +90,23 @@ class TimeSeriesLayer():
     def __find_datapoints_per_stat(self, tr, start, stop, time_range_in_hours,
                                    org, resource, metric, stat):
 
-        if not tr[self.__get_available_metrics_dir(tr, org).pack(
+        available_metrics = fdb.directory.create_or_open(
+            tr, ('monitoring', org, 'available_metrics'))
+        if not tr[available_metrics.pack(
                 (resource, metric))].present():
             error_msg = "Metric type: %s for resource: %s doesn't exist." % (
                 metric, resource)
             return error(404, error_msg)
-        metric_type_tuple = tr[self.__get_available_metrics_dir(tr, org).
+        metric_type_tuple = tr[available_metrics.
                                pack((resource, metric))]
         metric_type = fdb.tuple.unpack(metric_type_tuple)[0]
 
         datapoints = []
         resolution = time_range_to_resolution(time_range_in_hours)
-        for k, v in tr[self.__get_resource_resolution_dir(
-                tr, org, resource, resolution).pack(start):
-                self.__get_resource_resolution_dir(tr, org, resource,
-                                                   resolution).pack(stop)]:
-
+        datapoints_dir = fdb.directory.create_or_open(
+            tr, ('monitoring', org, resource, resolution))
+        for k, v in tr[datapoints_dir.pack(start):
+                       datapoints_dir.pack(stop)]:
             tuple_key = list(fdb.tuple.unpack(k))
             if time_range_in_hours <= 1:
                 tuple_value = list(fdb.tuple.unpack(v))
@@ -164,16 +124,15 @@ class TimeSeriesLayer():
     @fdb.transactional
     def write_datapoint(self, tr, org, resource, key, value,
                         resolution='second'):
+        datapoints_dir = fdb.directory.create_or_open(
+            tr, ('monitoring', org, resource, resolution))
         if config('CHECK_DUPLICATES'):
-            if not tr[self.__get_resource_resolution_dir(
-                    tr, org, resource, resolution).pack(key)].present():
-                tr[self.__get_resource_resolution_dir(
-                    tr, org, resource, resolution).pack(key)] = fdb.tuple.pack(
-                        (value,))
+            if not tr[datapoints_dir.pack(key)].present():
+                tr[datapoints_dir.pack(key)] = fdb.tuple.pack(
+                    (value,))
                 return True
             saved_value = fdb.tuple.unpack(
-                tr[self.__get_resource_resolution_dir(
-                    tr, org, resource, resolution).pack(key)])[0]
+                tr[datapoints_dir.pack(key)])[0]
             if saved_value != value:
                 log.error("key: %s already exists with a different value" %
                           str(key))
@@ -182,9 +141,8 @@ class TimeSeriesLayer():
                     "key: %s already exists with the same value" % str(key))
             return False
 
-        tr[self.__get_resource_resolution_dir(
-            tr, org, resource, resolution).pack(key)] = fdb.tuple.pack(
-                (value,))
+        tr[datapoints_dir.pack(key)] = fdb.tuple.pack(
+            (value,))
         return True
 
     @fdb.transactional
@@ -200,26 +158,26 @@ class TimeSeriesLayer():
         if not config('AGGREGATE_%s' % resolution.upper()):
             # log something
             return
-        tr.add(self.__get_resource_resolution_dir(
-            tr, org, resource, resolution).pack(
+        datapoints_dir = fdb.directory.create_or_open(
+            tr, ('monitoring', org, resource, resolution))
+        tr.add(datapoints_dir.pack(
             time_aggregate_tuple(metric, "count", dt, resolution)),
             struct.pack('<q', 1))
-        tr.add(self.__get_resource_resolution_dir(
-            tr, org, resource, resolution).pack(
+        tr.add(datapoints_dir.pack(
             time_aggregate_tuple(metric, "sum", dt, resolution)),
             struct.pack('<q', value))
-        tr.min(self.__get_resource_resolution_dir(
-            tr, org, resource, resolution).pack(
+        tr.min(datapoints_dir.pack(
             time_aggregate_tuple(metric, "min", dt, resolution)),
             struct.pack('<q', value))
-        tr.max(self.__get_resource_resolution_dir(
-            tr, org, resource, resolution).pack(
+        tr.max(datapoints_dir.pack(
             time_aggregate_tuple(metric, "max", dt, resolution)),
             struct.pack('<q', value))
 
     @fdb.transactional
     def add_metric(self, tr, org, metric, metric_type):
-        if not tr[self.__get_available_metrics_dir(tr, org).pack(
+        available_metrics = fdb.directory.create_or_open(
+            tr, ('monitoring', org, 'available_metrics'))
+        if not tr[available_metrics.pack(
                 metric)].present():
-            tr[self.__get_available_metrics_dir(tr, org).pack(
+            tr[available_metrics.pack(
                 metric)] = fdb.tuple.pack((metric_type,))
