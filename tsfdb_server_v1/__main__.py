c880ce0ac0115e398d402fd77b84641a860ba36c
Mist Gitlab Runner <gitlab.ops.mist.io@gmail.com>
2020-10-05 17:19:31 +0000

    Regenerate tsfdb-v1 after: Add main patch to config

--- tsfdb_server_v1/__main__.py
+++ tsfdb_server_v1/__main__.py
@@ -5,14 +5,13 @@ import connexion
 from tsfdb_server_v1 import encoder
 
 
-def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'TSFDB'},
-                pythonic_params=True)
-    app.run(port=8080)
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'TSFDB'},
+            pythonic_params=True)
+application = app.app
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080)
